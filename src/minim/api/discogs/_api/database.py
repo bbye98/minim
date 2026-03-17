a2c6c8ff8653cddac7de46b07e2de90e5253466a
Benjamin Ye <benjaminbobinye@gmail.com>
2026-03-16 23:15:34 -0400

    Finish implementing the Database API endpoints in the Discogs API

--- src/minim/api/discogs/_api/database.py
+++ src/minim/api/discogs/_api/database.py
@@ -24,6 +24,7 @@ class DatabaseAPI(DiscogsResourceAPI):
         "catno",
         "country",
     }
+    _SEARCH_RESOURCE_TYPES = {"release", "master", "artist", "label"}
 
     @TTLCache.cached_method(ttl="popularity")
     def get_release(
@@ -940,37 +941,344 @@ class DatabaseAPI(DiscogsResourceAPI):
         self._validate_number("label_id", label_id, int, 1)
         return self._client._request("GET", f"labels/{label_id}").json()
 
-    # def get_label_releases(
-    #     self,
-    #     label_id: int | str,
-    #     /,
-    #     *,
-    #     limit: int | None = None,
-    #     page: int | None = None,
-    # ) -> dict[str, Any]: ...
+    @TTLCache.cached_method(ttl="popularity")
+    def get_label_releases(
+        self,
+        label_id: int | str,
+        /,
+        *,
+        limit: int | None = None,
+        page: int | None = None,
+    ) -> dict[str, Any]:
+        """
+        `Database > All Label Releases <https://www.discogs.com
+        /developers/#page:database,
+        header:database-all-label-releases>`_: Get Discogs catalog
+        information for a label's releases.
 
-    # def search(
-    #     self,
-    #     query: str,
-    #     /,
-    #     *,
-    #     type_: str | None = None,
-    #     title: str | None = None,
-    #     release_title: str | None = None,
-    #     credit: str | None = None,
-    #     artist: str | None = None,
-    #     anv: str | None = None,
-    #     label: str | None = None,
-    #     genre: str | None = None,
-    #     style: str | None = None,
-    #     country: str | None = None,
-    #     year: int | str | None = None,
-    #     format_: str | None = None,
-    #     catno: str | None = None,
-    #     barcode: str | None = None,
-    #     track: str | None = None,
-    #     submitter: str | None = None,
-    #     contributor: str | None = None,
-    #     limit: int | None = None,
-    #     page: int | None = None,
-    # ) -> dict[str, Any]: ...
+        Parameters
+        ----------
+        label_id : int or str; positional-only
+            Discogs ID of the label.
+
+            **Examples**: :code:`1`, :code:`"681"`.
+
+        limit : int; keyword-only; optional
+            Maximum number of releases to return.
+
+            **Valid range**: :code:`1` to :code:`100`.
+
+            **API default**: :code:`50`.
+
+        page : int; keyword-only; optional
+            Page number. Use with `limit` to get the next page of
+            releases.
+
+            **Minimum value**: :code:`1`.
+
+            **API default**: :code:`1`.
+
+        Returns
+        -------
+        releases : dict[str, Any]
+            Page of Discogs content metadata for the label's releases.
+
+            .. admonition:: Sample response
+               :class: response dropdown
+
+               .. code::
+
+                  {
+                    "pagination": {
+                      "items": <int>,
+                      "page": <int>,
+                      "pages": <int>,
+                      "per_page": <int>,
+                      "urls": {
+                        "first": <str>,
+                        "last": <str>,
+                        "next": <str>,
+                        "prev": <str>
+                      }
+                    },
+                    "releases": [
+                      {
+                        "artist": <str>,
+                        "catno": <str>,
+                        "format": <str>,
+                        "id": <int>,
+                        "resource_url": <str>,
+                        "stats": {
+                          "community": {
+                            "in_collection": <int>,
+                            "in_wantlist": <int>
+                          },
+                          "user": {
+                            "in_collection": <int>,
+                            "in_wantlist": <int>
+                          }
+                        },
+                        "status": <str>,
+                        "thumb": <str>,
+                        "title": <str>,
+                        "year": <int>
+                      }
+                    ]
+        """
+        return self._get_paginated_resources(
+            f"labels/{label_id}/releases", limit=limit, page=page
+        )
+
+    def search(
+        self,
+        query: str | None = None,
+        /,
+        *,
+        resource_type: str | None = None,
+        title: str | None = None,
+        release: str | None = None,
+        artist: str | None = None,
+        artist_variation: str | None = None,
+        track: str | None = None,
+        credit: str | None = None,
+        label: str | None = None,
+        genre: str | None = None,
+        style: str | None = None,
+        release_country: str | None = None,
+        release_format: str | None = None,
+        release_year: int | str | None = None,
+        catalog_number: str | None = None,
+        barcode: str | None = None,
+        submitter: str | None = None,
+        contributor: str | None = None,
+        limit: int | None = None,
+        page: int | None = None,
+    ) -> dict[str, Any]:
+        """
+        `Database > Search <https://www.discogs.com/developers
+        /#page:database,header:database-search>`_: Search for artists,
+        releases, labels, and masters in the Discogs catalog.
+
+        .. admonition:: User authentication
+           :class: entitlement
+
+           .. tab-set::
+
+              .. tab-item:: Required
+
+                 User authentication
+                    Access protected endpoints.
+
+        Parameters
+        ----------
+        query : str; positional-only; optional
+            Search query.
+
+            **Example**: :code:`"Nirvana"`.
+
+        resource_type : str; keyword-only; optional
+            Resource type to filter search results by.
+
+            **Valid values**: :code:`"release"`, :code:`"master"`,
+            :code:`"artist"`, :code:`"label"`.
+
+        title : str; keyword-only; optional
+            Combined artist and title search field to filter search
+            results by.
+
+            **Example**: :code:`"Nirvana - Nevermind"`.
+
+        release : str; keyword-only; optional
+            Release title to filter search results by.
+
+            **Example**: :code:`"Nevermind"`.
+
+        artist : str; keyword-only; optional
+            Artist name to filter search results by.
+
+            **Example**: :code:`"Nirvana"`.
+
+        artist_variation : str; keyword-only; optional
+            Artist name variation to filter search results by.
+
+            **Example**: :code:`"Nirvana (US)"`.
+
+        track : str; keyword-only; optional
+            Track title to filter search results by.
+
+            **Example**: :code:`"Smells Like Teen Spirit"`.
+
+        credit : str; keyword-only; optional
+            Release credit to filter search results by.
+
+            **Example**: :code:`"Kurt Cobain"`.
+
+        label : str; keyword-only; optional
+            Label name to filter search results by.
+
+            **Example**: :code:`"DGC"`.
+
+        genre : str; keyword-only; optional
+            Genre to filter search results by.
+
+            **Example**: :code:`"Rock"`.
+
+        style : str; keyword-only; optional
+            Style to filter search results by.
+
+            **Example**: :code:`"Grunge"`.
+
+        release_country : str; keyword-only; optional
+            Release country to filter search results by.
+
+            **Example**: :code:`"Canada"`.
+
+        release_format : str; keyword-only; optional
+            Release format to filter search results by.
+
+            **Example**: :code:`"Album"`.
+
+        release_year : int or str; keyword-only; optional
+            Release year to filter search results by.
+
+            **Examples**: :code:`1991`, :code:`"1991"`.
+
+        catalog_number : str; keyword-only; optional
+            Catalog number to filter search results by.
+
+            **Example**: :code:`"DGC-24425"`.
+
+        barcode : str; keyword-only; optional
+            Release barcode to filter search results by.
+
+            **Example**: :code:`"7 2064-24425-2 4"`.
+
+        submitter : str; keyword-only; optional
+            Username of the Discogs user who submitted the release to
+            filter search results by.
+
+            **Example**: :code:`"milKt"`.
+
+        contributor : str; keyword-only; optional
+            Username of the Discogs user who contributed to the
+            release's metadata to filter search results by.
+
+            **Example**: :code:`"jerome99"`.
+
+        limit : int; keyword-only; optional
+            Maximum number of search results to return.
+
+            **Valid range**: :code:`1` to :code:`100`.
+
+            **API default**: :code:`50`.
+
+        page : int; keyword-only; optional
+            Page number. Use with `limit` to get the next page of search
+            results.
+
+            **Minimum value**: :code:`1`.
+
+            **API default**: :code:`1`.
+
+        Returns
+        -------
+        search_results : dict[str, Any]
+            Page of search results.
+
+            .. admonition:: Sample response
+               :class: response dropdown
+
+               .. code::
+
+                  {
+                    "pagination": {
+                      "items": <int>,
+                      "page": <int>,
+                      "pages": <int>,
+                      "per_page": <int>,
+                      "urls": {
+                        "first": <str>,
+                        "last": <str>,
+                        "next": <str>,
+                        "prev": <str>
+                      }
+                    },
+                    "results": [
+                      {
+                        "cover_image": <str>,
+                        "id": <int>,
+                        "master_id": <int>,
+                        "master_url": <str>,
+                        "resource_url": <str>,
+                        "thumb": <str>,
+                        "title": <str>,
+                        "type": <str>,
+                        "uri": <str>,
+                        "user_data": {
+                          "in_collection": <bool>,
+                          "in_wantlist": <bool>
+                        }
+                      }
+                    ]
+                  }
+        """
+        self._client._require_authentication("database.search")
+        params = {}
+        if query is not None:
+            params["query"] = self._prepare_string("query", query)
+        if resource_type is not None:
+            resource_type = self._prepare_string(
+                "resource_type", resource_type
+            ).lower()
+            if resource_type not in self._SEARCH_RESOURCE_TYPES:
+                raise ValueError(
+                    f"Invalid resource type {resource_type!r}. Valid values: "
+                    f"{self._join_values(self._SEARCH_RESOURCE_TYPES)}."
+                )
+            params["type"] = resource_type
+        if title is not None:
+            params["title"] = self._prepare_string("title", title)
+        if release is not None:
+            params["release_title"] = self._prepare_string("release", release)
+        if artist is not None:
+            params["artist"] = self._prepare_string("artist", artist)
+        if artist_variation is not None:
+            params["artist_variation"] = self._prepare_string(
+                "artist_variation", artist_variation
+            )
+        if track is not None:
+            params["track"] = self._prepare_string("track", track)
+        if credit is not None:
+            params["credit"] = self._prepare_string("credit", credit)
+        if label is not None:
+            params["label"] = self._prepare_string("label", label)
+        if genre is not None:
+            params["genre"] = self._prepare_string("genre", genre)
+        if style is not None:
+            params["style"] = self._prepare_string("style", style)
+        if release_country is not None:
+            params["country"] = self._prepare_string(
+                "release_country", release_country
+            )
+        if release_format is not None:
+            params["format"] = self._prepare_string(
+                "release_format", release_format
+            )
+        if release_year is not None:
+            self._validate_numeric("release_year", release_year, int, 0)
+            params["year"] = str(release_year)
+        if catalog_number is not None:
+            params["catno"] = self._prepare_string(
+                "catalog_number", catalog_number
+            )
+        if barcode is not None:
+            params["barcode"] = self._prepare_barcode("barcode", barcode)
+        if submitter is not None:
+            params["submitter"] = self._prepare_string("submitter", submitter)
+        if contributor is not None:
+            params["contributor"] = self._prepare_string(
+                "contributor", contributor
+            )
+        return self._get_paginated_resources(
+            "database/search", limit=limit, page=page, params=params
+        )
