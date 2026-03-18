687644102e80b9c0183155eacee11edaaea4a6a5
Benjamin Ye <benjaminbobinye@gmail.com>
2026-03-17 22:43:31 -0400

    Update documentation

--- src/minim/api/spotify/_web_api/playlists.py
+++ src/minim/api/spotify/_web_api/playlists.py
@@ -1091,7 +1091,7 @@ class PlaylistsAPI(SpotifyResourceAPI):
                  [
                      "spotify:track:4iV5W9uYEdYUVa79Axb7Rh",
                      "spotify:track:1301WleyT98MSxVHPZCA6M",
-                        "spotify:episode:512ojhOuo1ktJprKbVcKyQ",
+                     "spotify:episode:512ojhOuo1ktJprKbVcKyQ",
                  ]
 
         snapshot_id : str; keyword-only; optional
