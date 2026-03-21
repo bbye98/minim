ca02e0f959f0cb7520fcd00a6ff54198ac352d62
bbye98 <benjaminbobinye@gmail.com>
2026-03-20 20:15:17 -0400

    Move Library API logic to Users API module

--- src/minim/api/spotify/_web_api/library.py
+++ src/minim/api/spotify/_web_api/library.py
@@ -1,4 +1,6 @@
+from ..._shared import _copy_docstring
 from ._shared import SpotifyResourceAPI
+from .users import UsersAPI
 
 
 class LibraryAPI(SpotifyResourceAPI):
@@ -12,179 +14,14 @@ class LibraryAPI(SpotifyResourceAPI):
        instantiated directly.
     """
 
+    @_copy_docstring(UsersAPI.save_items)
     def save_items(self, spotify_uris: str | list[str], /) -> None:
-        """
-        `Library > Save Items to Library <https://developer.spotify.com
-        /documentation/web-api/reference/save-library-items>`_: Save one
-        or more items to the current user's library.
+        self._client.users.save_items(spotify_uris)
 
-        .. admonition:: Authorization scope
-           :class: entitlement
-
-           .. tab-set::
-
-              .. tab-item:: Required
-
-                 :code:`user-library-modify` scope
-                    Manage your saved content. `Learn more.
-                    <https://developer.spotify.com/documentation/web-api
-                    /concepts/scopes#user-library-modify>`__
-
-                 :code:`user-follow-modify` scope
-                    Manage your saved content. `Learn more.
-                    <https://developer.spotify.com/documentation/web-api
-                    /concepts/scopes#user-follow-modify>`__
-
-                 :code:`playlist-modify-public` scope
-                    Manage your public playlists. `Learn more.
-                    <https://developer.spotify.com/documentation/web-api
-                    /concepts/scopes#playlist-modify-public>`__
-
-        Parameters
-        ----------
-        spotify_uris : str or list[str]; positional-only
-            Comma-separated string or list of Spotify URIs. A maximum of
-            40 URIs can be sent in a request.
-        """
-        self._client._request(
-            "PUT",
-            "me/library",
-            params={
-                "uris": ",".join(
-                    self._prepare_spotify_uris(
-                        spotify_uris,
-                        limit=40,
-                        resource_types={
-                            "track",
-                            "album",
-                            "episode",
-                            "show",
-                            "audiobook",
-                            "user",
-                            "playlist",
-                        },
-                    )
-                )
-            },
-        )
-
+    @_copy_docstring(UsersAPI.remove_saved_items)
     def remove_saved_items(self, spotify_uris: str | list[str], /) -> None:
-        """
-        `Library > Remove Items from Library
-        <https://developer.spotify.com/documentation/web-api/reference
-        /remove-library-items>`_: Remove one or more items from the
-        current user's library.
+        self._client.users.remove_saved_items(spotify_uris)
 
-        .. admonition:: Authorization scope
-           :class: entitlement
-
-           .. tab-set::
-
-              .. tab-item:: Required
-
-                 :code:`user-library-modify` scope
-                    Manage your saved content. `Learn more.
-                    <https://developer.spotify.com/documentation/web-api
-                    /concepts/scopes#user-library-modify>`__
-
-                 :code:`user-follow-modify` scope
-                    Manage your saved content. `Learn more.
-                    <https://developer.spotify.com/documentation/web-api
-                    /concepts/scopes#user-follow-modify>`__
-
-                 :code:`playlist-modify-public` scope
-                    Manage your public playlists. `Learn more.
-                    <https://developer.spotify.com/documentation/web-api
-                    /concepts/scopes#playlist-modify-public>`__
-
-        Parameters
-        ----------
-        spotify_uris : str or list[str]; positional-only
-            Comma-separated string or list of Spotify URIs. A maximum of
-            40 URIs can be sent in a request.
-        """
-        self._client._request(
-            "DELETE",
-            "me/library",
-            params={
-                "uris": ",".join(
-                    self._prepare_spotify_uris(
-                        spotify_uris,
-                        limit=40,
-                        resource_types={
-                            "track",
-                            "album",
-                            "episode",
-                            "show",
-                            "audiobook",
-                            "user",
-                            "playlist",
-                        },
-                    )
-                )
-            },
-        )
-
+    @_copy_docstring(UsersAPI.are_items_saved)
     def are_items_saved(self, spotify_uris: str | list[str], /) -> list[bool]:
-        """
-        `Library > Check User's Saved Items
-        <https://developer.spotify.com/documentation/web-api/reference
-        /check-library-contains>`_: Check whether one or more items are
-        saved in the current user's library.
-
-        .. admonition:: Authorization scope
-           :class: entitlement
-
-           .. tab-set::
-
-              .. tab-item:: Required
-
-                 :code:`user-library-read` scope
-                    Access your saved content. `Learn more.
-                    <https://developer.spotify.com/documentation/web-api
-                    /concepts/scopes#user-library-read>`__
-
-                 :code:`user-follow-read` scope
-                    Access your followers and who you are following.
-                    `Learn more. <https://developer.spotify.com
-                    /documentation/web-api/concepts
-                    /scopes#user-follow-read>`__
-
-                 :code:`playlist-read-private` scope
-                    Access your private playlists. `Learn more.
-                    <https://developer.spotify.com/documentation/web-api
-                    /concepts/scopes#playlist-read-private>`__
-
-        Parameters
-        ----------
-        spotify_uris : str or list[str]; positional-only
-            Comma-separated string or list of Spotify URIs. A maximum of
-            40 URIs can be sent in a request.
-
-        Returns
-        -------
-        saved : list[bool]
-            Whether the current user has the specified albums saved in
-            their library.
-        """
-        return self._client._request(
-            "GET",
-            "me/library/contains",
-            params={
-                "ids": ",".join(
-                    self._prepare_spotify_uris(
-                        spotify_uris,
-                        limit=40,
-                        resource_types={
-                            "track",
-                            "album",
-                            "episode",
-                            "show",
-                            "audiobook",
-                            "user",
-                            "playlist",
-                        },
-                    )
-                )
-            },
-        )
+        return self._client.users.are_items_saved(spotify_uris)
--- src/minim/api/spotify/_web_api/users.py
+++ src/minim/api/spotify/_web_api/users.py
@@ -17,6 +17,15 @@ class UsersAPI(SpotifyResourceAPI):
     """
 
     _PEOPLE_TYPES = {"artists", "users"}
+    _RESOURCE_TYPES = {
+        "track",
+        "album",
+        "episode",
+        "show",
+        "audiobook",
+        "user",
+        "playlist",
+    }
     _TIME_RANGES = {"long_term", "medium_term", "short_term"}
 
     @classmethod
@@ -2882,3 +2891,181 @@ class UsersAPI(SpotifyResourceAPI):
             "tracks.are_tracks_saved", "user-library-read"
         )
         return self._are_entities_saved("tracks", track_ids)
+
+    def save_items(self, spotify_uris: str | list[str], /) -> None:
+        """
+        `Library > Save Items to Library <https://developer.spotify.com
+        /documentation/web-api/reference/save-library-items>`_: Save one
+        or more items to the current user's library.
+
+        .. admonition:: Authorization scope
+           :class: entitlement
+
+           .. tab-set::
+
+              .. tab-item:: Required
+
+                 :code:`user-library-modify` scope
+                    Manage your saved content. `Learn more.
+                    <https://developer.spotify.com/documentation/web-api
+                    /concepts/scopes#user-library-modify>`__
+
+                 :code:`user-follow-modify` scope
+                    Manage your saved content. `Learn more.
+                    <https://developer.spotify.com/documentation/web-api
+                    /concepts/scopes#user-follow-modify>`__
+
+                 :code:`playlist-modify-public` scope
+                    Manage your public playlists. `Learn more.
+                    <https://developer.spotify.com/documentation/web-api
+                    /concepts/scopes#playlist-modify-public>`__
+
+        Parameters
+        ----------
+        spotify_uris : str or list[str]; positional-only
+            Comma-separated string or list of Spotify URIs. A maximum of
+            40 URIs can be sent in a request.
+        """
+        self._client._require_scopes(
+            "library.save_items",
+            {
+                "user-library-modify",
+                "user-follow-modify",
+                "playlist-modify-public",
+            },
+        )
+        self._client._request(
+            "PUT",
+            "me/library",
+            params={
+                "uris": ",".join(
+                    self._prepare_spotify_uris(
+                        spotify_uris,
+                        limit=40,
+                        resource_types=self._RESOURCE_TYPES,
+                    )
+                )
+            },
+        )
+
+    def remove_saved_items(self, spotify_uris: str | list[str], /) -> None:
+        """
+        `Library > Remove Items from Library
+        <https://developer.spotify.com/documentation/web-api/reference
+        /remove-library-items>`_: Remove one or more items from the
+        current user's library.
+
+        .. admonition:: Authorization scope
+           :class: entitlement
+
+           .. tab-set::
+
+              .. tab-item:: Required
+
+                 :code:`user-library-modify` scope
+                    Manage your saved content. `Learn more.
+                    <https://developer.spotify.com/documentation/web-api
+                    /concepts/scopes#user-library-modify>`__
+
+                 :code:`user-follow-modify` scope
+                    Manage your saved content. `Learn more.
+                    <https://developer.spotify.com/documentation/web-api
+                    /concepts/scopes#user-follow-modify>`__
+
+                 :code:`playlist-modify-public` scope
+                    Manage your public playlists. `Learn more.
+                    <https://developer.spotify.com/documentation/web-api
+                    /concepts/scopes#playlist-modify-public>`__
+
+        Parameters
+        ----------
+        spotify_uris : str or list[str]; positional-only
+            Comma-separated string or list of Spotify URIs. A maximum of
+            40 URIs can be sent in a request.
+        """
+        self._client._require_scopes(
+            "library.remove_saved_items",
+            {
+                "user-library-modify",
+                "user-follow-modify",
+                "playlist-modify-public",
+            },
+        )
+        self._client._request(
+            "DELETE",
+            "me/library",
+            params={
+                "uris": ",".join(
+                    self._prepare_spotify_uris(
+                        spotify_uris,
+                        limit=40,
+                        resource_types=self._RESOURCE_TYPES,
+                    )
+                )
+            },
+        )
+
+    @TTLCache.cached_method(ttl="user")
+    def are_items_saved(self, spotify_uris: str | list[str], /) -> list[bool]:
+        """
+        `Library > Check User's Saved Items
+        <https://developer.spotify.com/documentation/web-api/reference
+        /check-library-contains>`_: Check whether one or more items are
+        saved in the current user's library.
+
+        .. admonition:: Authorization scope
+           :class: entitlement
+
+           .. tab-set::
+
+              .. tab-item:: Required
+
+                 :code:`user-library-read` scope
+                    Access your saved content. `Learn more.
+                    <https://developer.spotify.com/documentation/web-api
+                    /concepts/scopes#user-library-read>`__
+
+                 :code:`user-follow-read` scope
+                    Access your followers and who you are following.
+                    `Learn more. <https://developer.spotify.com
+                    /documentation/web-api/concepts
+                    /scopes#user-follow-read>`__
+
+                 :code:`playlist-read-private` scope
+                    Access your private playlists. `Learn more.
+                    <https://developer.spotify.com/documentation/web-api
+                    /concepts/scopes#playlist-read-private>`__
+
+        Parameters
+        ----------
+        spotify_uris : str or list[str]; positional-only
+            Comma-separated string or list of Spotify URIs. A maximum of
+            40 URIs can be sent in a request.
+
+        Returns
+        -------
+        saved : list[bool]
+            Whether the current user has the specified albums saved in
+            their library.
+        """
+        self._client._require_scopes(
+            "library.are_items_saved",
+            {
+                "user-library-read",
+                "user-follow-read",
+                "playlist-read-private",
+            },
+        )
+        return self._client._request(
+            "GET",
+            "me/library/contains",
+            params={
+                "ids": ",".join(
+                    self._prepare_spotify_uris(
+                        spotify_uris,
+                        limit=40,
+                        resource_types=self._RESOURCE_TYPES,
+                    )
+                )
+            },
+        )
